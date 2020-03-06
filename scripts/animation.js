90d47decc8cd7974eb007f80c74ca0829e6ce402
brendon silva <brendon.silva>
2020-03-06 10:47:53 -0300

    init github

---
+++ scripts/animation.js
@@ -0,0 +1,105 @@
+TweenMax.from(".voice-of-sea", 2, {
+  delay: 2,
+  opacity: 0,
+  ease: Expo.easeInOut
+})
+
+TweenMax.to(".voice-of-sea", 2, {
+  delay: 4,
+  opacity: 0,
+  y: -60,
+  ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlay h1", 2, {
+  delay: 4,
+  opacity: 0,
+  y: -60,
+  ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlay", 2, {
+  delay: 5,
+  top: "-100%",
+  ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlay span", 2, {
+  delay: 5.3,
+  opacity: 0,
+  y: -60,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".ellipse-container", 1, {
+  delay: 7,
+  opacity: 0,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".yellow", 1, {
+  delay: 7.5,
+  opacity: 0,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".circle1", 1, {
+  delay: 8,
+  opacity: 0,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".circle2", 1, {
+  delay: 8,
+  opacity: 0,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".logo", 1, {
+  delay: 8.5,
+  opacity: 0,
+  y: -100,
+  ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".menu-links ul li", 1, {
+  delay: 9.2,
+  opacity: 0,
+  x: -100,
+  ease: Expo.easeInOut
+}, 0.08)
+
+TweenMax.from(".scrolldown", 1, {
+  delay: 9.3,
+  opacity: 0,
+  y: 100,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".text .title", 1, {
+  delay: 9,
+  opacity: 0,
+  x: 200,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".text p", 1, {
+  delay: 9.2,
+  opacity: 0,
+  x: 200,
+  ease: Expo.easeInOut
+})
+
+TweenMax.from(".watchnow", 1, {
+  delay: 9.4,
+  opacity: 0,
+  x: 200,
+  ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".media ul li", 1, {
+  delay: 9,
+  opacity: 0,
+  y: 100,
+  ease: Expo.easeInOut
+}, 0.08)
