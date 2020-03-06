4a11772f1f1394f98d7933bc9508b3245ae187d1
brendon silva <brendon.silva>
2020-03-06 11:06:14 -0300

    removed type-text-

--- scripts/animation.js
+++ scripts/animation.js
@@ -5,100 +5,100 @@ TweenMax.from(".voice-of-sea", 2, {
 })
 
 TweenMax.to(".voice-of-sea", 2, {
-  delay: 4,
+  delay: 2,
   opacity: 0,
   y: -60,
   ease: Expo.easeInOut
 })
 
 TweenMax.to(".overlay h1", 2, {
-  delay: 4,
+  delay: 2,
   opacity: 0,
   y: -60,
   ease: Expo.easeInOut
 })
 
 TweenMax.to(".overlay", 2, {
-  delay: 5,
+  delay: 3,
   top: "-100%",
   ease: Expo.easeInOut
 })
 
 TweenMax.to(".overlay span", 2, {
-  delay: 5.3,
+  delay: 3.3,
   opacity: 0,
   y: -60,
   ease: Expo.easeInOut
 })
 
 TweenMax.from(".ellipse-container", 1, {
-  delay: 7,
+  delay: 5,
   opacity: 0,
   ease: Expo.easeInOut
 })
 
 TweenMax.from(".yellow", 1, {
-  delay: 7.5,
+  delay: 5.5,
   opacity: 0,
   ease: Expo.easeInOut
 })
 
 TweenMax.from(".circle1", 1, {
-  delay: 8,
+  delay: 6,
   opacity: 0,
   ease: Expo.easeInOut
 })
 
 TweenMax.from(".circle2", 1, {
-  delay: 8,
+  delay: 6,
   opacity: 0,
   ease: Expo.easeInOut
 })
 
 TweenMax.from(".logo", 1, {
-  delay: 8.5,
+  delay: 6.5,
   opacity: 0,
   y: -100,
   ease: Expo.easeInOut
 })
 
 TweenMax.staggerFrom(".menu-links ul li", 1, {
-  delay: 9.2,
+  delay: 7.2,
   opacity: 0,
   x: -100,
   ease: Expo.easeInOut
 }, 0.08)
 
 TweenMax.from(".scrolldown", 1, {
-  delay: 9.3,
+  delay: 7.3,
   opacity: 0,
   y: 100,
   ease: Expo.easeInOut
 })
 
 TweenMax.from(".text .title", 1, {
-  delay: 9,
+  delay: 7,
   opacity: 0,
   x: 200,
   ease: Expo.easeInOut
 })
 
 TweenMax.from(".text p", 1, {
-  delay: 9.2,
+  delay: 7.2,
   opacity: 0,
   x: 200,
   ease: Expo.easeInOut
 })
 
 TweenMax.from(".watchnow", 1, {
-  delay: 9.4,
+  delay: 7.4,
   opacity: 0,
   x: 200,
   ease: Expo.easeInOut
 })
 
 TweenMax.staggerFrom(".media ul li", 1, {
-  delay: 9,
+  delay: 7,
   opacity: 0,
   y: 100,
   ease: Expo.easeInOut
--- scripts/type-text.js
+++ scripts/type-text.js
@@ -1,82 +1,82 @@
-var $cursor = $("#cursor"),
-  cursorInterval = 0.1,
-  mySplitText = new SplitText("#text", {
-    type: "words,chars",
-    charsClass: "chars",
-    position: "absolute"
-  }),
-  $chars = $(".chars"),
-  tl = new TimelineMax({
-    paused: true,
-    delay: 1,
-    // comment out yoyo, repeat, repeatDelay, and onRepeat below if no reverse is needed
-    yoyo: true,
-    repeat: -1,
-    repeatDelay: 2,
-    onRepeat: function () {
-      //stops blinking
-      TweenLite.set($cursor, {
-        autoAlpha: 1
-      });
-    }
-  });
+// var $cursor = $("#cursor"),
+//   cursorInterval = 0.1,
+//   mySplitText = new SplitText("#text", {
+//     type: "words,chars",
+//     charsClass: "chars",
+//     position: "absolute"
+//   }),
+//   $chars = $(".chars"),
+//   tl = new TimelineMax({
+//     paused: true,
+//     delay: 1,
+//     // comment out yoyo, repeat, repeatDelay, and onRepeat below if no reverse is needed
+//     yoyo: true,
+//     repeat: -1,
+//     repeatDelay: 2,
+//     onRepeat: function () {
+//       //stops blinking
+//       TweenLite.set($cursor, {
+//         autoAlpha: 1
+//       });
+//     }
+//   });
 
-TweenLite.set("#textWrapper", {
-  visibility: "visible"
-});
+// TweenLite.set("#textWrapper", {
+//   visibility: "visible"
+// });
 
-//enable the blinking cursor
-function blinkingCursor() {
-  TweenMax.fromTo($cursor, 0.5, {
-    autoAlpha: 0
-  }, {
-    autoAlpha: 1,
-    repeat: -1,
-    ease: SteppedEase.config(1)
-  })
-}
+// //enable the blinking cursor
+// function blinkingCursor() {
+//   TweenMax.fromTo($cursor, 0.5, {
+//     autoAlpha: 0
+//   }, {
+//     autoAlpha: 1,
+//     repeat: -1,
+//     ease: SteppedEase.config(1)
+//   })
+// }
 
-// this makes the cursor resume blinking after reversing
-tl.call(blinkingCursor);
+// // this makes the cursor resume blinking after reversing
+// tl.call(blinkingCursor);
 
-// this makes it visible (not blinking) when playing forward
-tl.set($cursor, {
-  autoAlpha: 1
-}, 0.1);
+// // this makes it visible (not blinking) when playing forward
+// tl.set($cursor, {
+//   autoAlpha: 1
+// }, 0.1);
 
-//loop through all the chars and make them visible AND set cursor to their right at same time
-$chars.each(function (i, el) {
+// //loop through all the chars and make them visible AND set cursor to their right at same time
+// $chars.each(function (i, el) {
 
-  var $char = $(el),
-    offset = $char.offset(),
-    width = $char.width(),
-    leftExtra = 400,
-    topExtra = 400;
+//   var $char = $(el),
+//     offset = $char.offset(),
+//     width = $char.width(),
+//     leftExtra = 400,
+//     topExtra = 400;
 
-  // set initial $cursor CSS properties
-  // you may have to play with leftExtra and topExtra for alignment
-  tl.set($cursor, {
-    left: (offset.left + width) - leftExtra,
-    top: (offset.top - topExtra)
-  }, (i + 1) * cursorInterval);
+//   // set initial $cursor CSS properties
+//   // you may have to play with leftExtra and topExtra for alignment
+//   tl.set($cursor, {
+//     left: (offset.left + width) - leftExtra,
+//     top: (offset.top - topExtra)
+//   }, (i + 1) * cursorInterval);
 
-  // set initial $char CSS properties
-  tl.set($char, {
-    autoAlpha: 1
-  }, ((i + 1) * cursorInterval));
-});
+//   // set initial $char CSS properties
+//   tl.set($char, {
+//     autoAlpha: 1
+//   }, ((i + 1) * cursorInterval));
+// });
 
-// re-call blinkingCursor function after the last char is displayed
-tl.call(blinkingCursor);
+// // re-call blinkingCursor function after the last char is displayed
+// tl.call(blinkingCursor);
 
-// control speed of typing
-tl.timeScale(0.6);
+// // control speed of typing
+// tl.timeScale(0.6);
 
-// start blinkingCursor before playing timeline
-blinkingCursor();
+// // start blinkingCursor before playing timeline
+// blinkingCursor();
 
-// play timleine after 1 second delay
-TweenMax.delayedCall(1, function () {
-  tl.progress(1).progress(0);
-  tl.play();
-});
+// // play timleine after 1 second delay
+// TweenMax.delayedCall(1, function () {
+//   tl.progress(1).progress(0);
+//   tl.play();
+// });
